e319bef6c741d7548dcbd946d1d6fdfa3e366492
GuninaDD <dasha.gunina@g,ail.vom>
2019-09-19 19:47:29 +0300

    Final step

---
+++ app/src/main/java/com/simbirsoft/wordpressclient/ui/posts/ViewPostActivity.java
@@ -0,0 +1,28 @@
+package com.simbirsoft.wordpressclient.ui.posts;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.simbirsoft.wordpressclient.R;
+
+public class ViewPostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.screen_posts_view);
+        Toolbar toolbar = findViewById(R.id.toolbar_viewposts);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.menu_postview, menu);
+        return true;
+    }
+}
